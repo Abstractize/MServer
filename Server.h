f78d3230360e2c7527a515e2636085d712a2d76e
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2018-09-14 14:52:29 -0600

    Created ADT

---
+++ Server.h
@@ -0,0 +1,14 @@
+//
+// Created by gabriel on 08/09/18.
+//
+
+#ifndef UNTITLED1_SERVER_H
+#define UNTITLED1_SERVER_H
+
+
+class Server {
+
+};
+
+
+#endif //UNTITLED1_SERVER_H
---
+++ ServerThread.h
@@ -0,0 +1,14 @@
+//
+// Created by gabriel on 08/09/18.
+//
+
+#ifndef UNTITLED1_SERVERTHREAD_H
+#define UNTITLED1_SERVERTHREAD_H
+
+
+class ServerThread {
+
+};
+
+
+#endif //UNTITLED1_SERVERTHREAD_H
