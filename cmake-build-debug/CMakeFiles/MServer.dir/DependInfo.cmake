7ed1e7b469ca7a6ba0ba20be44b2f78edbc19d5c
Gabriel Abarca Aguilar <gababarca@hotmail.com>
2018-10-04 23:21:49 -0600

    Server Complete

---
+++ cmake-build-debug/CMakeFiles/MServer.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/abstractize/Desktop/MServer/MServer/AbstractDataType/DoubleList.cpp" "/home/abstractize/Desktop/MServer/MServer/cmake-build-debug/CMakeFiles/MServer.dir/AbstractDataType/DoubleList.cpp.o"
+  "/home/abstractize/Desktop/MServer/MServer/AbstractDataType/List.cpp" "/home/abstractize/Desktop/MServer/MServer/cmake-build-debug/CMakeFiles/MServer.dir/AbstractDataType/List.cpp.o"
+  "/home/abstractize/Desktop/MServer/MServer/MServer/ServerB.cpp" "/home/abstractize/Desktop/MServer/MServer/cmake-build-debug/CMakeFiles/MServer.dir/MServer/ServerB.cpp.o"
+  "/home/abstractize/Desktop/MServer/MServer/MServer/ServerThread.cpp" "/home/abstractize/Desktop/MServer/MServer/cmake-build-debug/CMakeFiles/MServer.dir/MServer/ServerThread.cpp.o"
+  "/home/abstractize/Desktop/MServer/MServer/main.cpp" "/home/abstractize/Desktop/MServer/MServer/cmake-build-debug/CMakeFiles/MServer.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MServer.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MServer.dir/main.cpp.o"
+  "CMakeFiles/MServer.dir/MServer/ServerThread.cpp.o"
+  "CMakeFiles/MServer.dir/AbstractDataType/DoubleList.cpp.o"
+  "CMakeFiles/MServer.dir/MServer/ServerB.cpp.o"
+  "CMakeFiles/MServer.dir/AbstractDataType/List.cpp.o"
+  "MServer.pdb"
+  "MServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
